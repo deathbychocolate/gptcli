523a03e9a696079f94881e45ab7c52fafcb6b504
Damianos Vonapartis <damianosvon@gmail.com>
2023-03-05 00:27:48 +0100

    feat(openai): rename file and add skeleton code

--- src/main/openai_wrapper.py
+++ src/main/openai_wrapper.py
@@ -67,8 +67,7 @@ class OpenAIWrapper:
         """
 
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages
+            model="gpt-3.5-turbo", messages=messages
         )
 
         return response["choices"][0]["message"]["content"]
