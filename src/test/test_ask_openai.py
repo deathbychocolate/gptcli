f9f035b06b12eb850da99560d74e81d691a58f92
Damianos Vonapartis <damianosvon@gmail.com>
2023-03-15 22:14:18 +0100

    clean(tests): add some test structure

---
+++ src/test/test_ask_openai.py
@@ -0,0 +1,29 @@
+"""
+File to hold tests related to AskOpenAI
+"""
+
+import boto3
+import pytest
+
+
+class TestMessage:
+
+    def test_should_return_a_dictionary():
+        assert False
+        # dictionary() -> dict:
+
+class TestMessageFactory:
+
+    def test_should_create_a_message():
+        assert False
+        # create_message(, role: str, content: str) -> dict:
+
+class TestAskOpenAI:
+
+    def test_should_ask():
+        assert False
+        # ask() -> str:
+    
+    def test_should__build_messages():
+        assert False
+        # _build_messages(, question: str) -> list:
---
+++ src/test/test_text_to_speech.py
@@ -0,0 +1,53 @@
+"""
+File that contains all tests related to TextToSpeech
+"""
+
+import boto3
+import pytest
+
+from src.main.text_to_speech import TextToSpeech
+
+
+def test_should_generate_and_store_audio_stream_to_file():
+    assert False
+    # generate_and_store_audio_stream_to_file() -> str:
+
+
+def test_should_generate_and_play_audio_stream():
+    assert False
+    # generate_and_play_audio_stream() -> None:
+
+
+def test_should__generate_audio_stream():
+    assert False
+    # _generate_audio_stream() -> bytes:
+
+
+def test_should__generate_polly_client():
+    assert False
+    # _generate_polly_client() -> boto3.client:
+
+
+def test_should__store_audio_stream_to_file():
+    assert False
+    # _store_audio_stream_to_file(, stream: bytes) -> str:
+
+
+def test_should__generate_file_name():
+    assert False
+    # _generate_file_name() -> str:
+
+
+def test_should_write_audio_to_file():
+    assert False
+    # write_audio_to_file(, filename: str, stream: bytes) -> None:
+
+
+def test_should_play_audio_stream():
+    assert False
+    # play_audio_stream(, stream: bytes) -> None:
+
+
+def test_should_play_audio_file():
+    assert False
+    # play_audio_file(, filename: str) -> None:
