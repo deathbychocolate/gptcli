0f8d08a57fcd747d06a798adf3f32db0b8df51db
Damianos Vonapartis <damianosvon@gmail.com>
2023-03-19 00:27:06 +0100

    test: write some tests

--- src/test/test_openai_helper.py
+++ src/test/test_openai_helper.py
@@ -2,28 +2,66 @@
 File to hold tests related to AskOpenAI
 """
 
-import boto3
 import pytest
+
+from src.main.openai_helper import Message
+from src.main.openai_helper import MessageFactory
+from src.main.openai_helper import OpenAIHelper
 
 
 class TestMessage:
+    @pytest.fixture(scope="session")
+    def setup(self):
+        message = Message("user", "Hello")
+        return message.dictionary
 
-    def test_should_return_a_dictionary():
-        assert False
-        # dictionary() -> dict:
+    def test_should_return_a_type_dictionary(self, setup):
+        dictionary = setup
+        assert type(dictionary) is dict
+
+    def test_should_return_a_dictionary_with_a_role_key(self, setup):
+        dictionary = setup
+        assert dictionary.get("role") is not None
+
+    def test_should_return_a_dictionary_with_a_content_key(self, setup):
+        dictionary = setup
+        assert dictionary.get("content") is not None
+
 
 class TestMessageFactory:
 
-    def test_should_create_a_message():
-        assert False
-        # create_message(, role: str, content: str) -> dict:
+    @pytest.fixture(scope="session")
+    def setup(self):
+        factory = MessageFactory()
+        return factory
 
-class TestAskOpenAI:
+    def test_should_create_a_message(self, setup):
+        factory = setup
+        message = factory.create_message("user", "Hello")
+        assert type(message) is Message
 
-    def test_should_ask():
-        assert False
-        # ask() -> str:
-    
-    def test_should__build_messages():
+
+class TestOpenAIHelper:
+    @pytest.fixture(scope="session")
+    def setup(self):
+        helper = OpenAIHelper(OpenAIHelper.DEFAULT_ENGINE, "Good morning!")
+        return helper
+
+    def test_should_return_a_type_string(self, setup):
+        helper = setup
+        reply = helper.send()
+        assert type(reply) is str
+
+    def test_should_create_chat_completion(self, setup):
+        helper = setup
+        chat_completion = helper._create_chat_completion()
+        assert type(chat_completion) is dict
+
+    def test_should_retrieve_chat_completion_content(self, setup):
+        helper = setup
+        content = helper._retrieve_chat_completion_content()
+        assert type(content) is str
+
+    def test_should_build_messages(self, setup):
         assert False
         # _build_messages(, question: str) -> list:
