78cc12fa4b5fa661b99c3ad5ba9b08ef477f9880
Damianos Vonapartis <damianosvon@gmail.com>
2023-04-02 17:51:01 +0200

    clean(test_chat): one class per method tested

--- src/test/test_chat.py
+++ src/test/test_chat.py
@@ -8,7 +8,7 @@ from src.main.openai_helper import OpenAIHelper
 from src.main.chat import Chat
 
 
-class TestChat:
+class TestHandleChatCommands:
     @pytest.fixture(scope="session")
     def setup_teardown(self):
         gpt_3_5 = OpenAIHelper.GPT_ALL[0]
@@ -16,13 +16,13 @@ class TestChat:
 
         yield chat_session
 
-    def test_handle_chat_commands_should_accept_an_exit_string_and_perform_a_system_exit(self, setup_teardown):
+    def test_should_accept_an_exit_string_and_perform_a_system_exit(self, setup_teardown):
         chat_session = setup_teardown
         with pytest.raises(SystemExit):
             chat_session._handle_chat_commands(user_input="exit")
         assert True
 
-    def test_handle_chat_commands_should_accept_an_empty_string_and_not_perform_a_system_exit(self, setup_teardown):
+    def test_should_accept_an_empty_string_and_not_perform_a_system_exit(self, setup_teardown):
         chat_session = setup_teardown
         chat_session._handle_chat_commands(user_input="")
         assert True
