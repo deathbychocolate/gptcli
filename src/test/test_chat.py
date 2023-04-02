2631cfad36cc038ca1d941040cfe9d52e1d8378c
Damianos Vonapartis <damianosvon@gmail.com>
2023-04-02 18:46:40 +0200

    clean(test_chat): add tests for prompt user

--- src/test/test_chat.py
+++ src/test/test_chat.py
@@ -26,3 +26,28 @@ class TestHandleChatCommands:
         chat_session = setup_teardown
         chat_session._handle_chat_commands(user_input="")
         assert True
+
+
+class TestPromptUser:
+    @pytest.fixture(scope="session")
+    def setup_teardown(self):
+        gpt_3_5 = OpenAIHelper.GPT_ALL[0]
+        chat_session = Chat(model=gpt_3_5)
+
+        yield chat_session
+
+    def test_should_accept_keyboard_interrupt_and_perform_a_system_exit(self, setup_teardown, monkeypatch):
+        chat_session = setup_teardown
+        # TODO monkeypatch does not work this way, fix it
+        monkeypatch.setattr('builtins.input', lambda _: KeyboardInterrupt)
+        with pytest.raises(SystemExit):
+            chat_session._prompt_user()
+        assert True
+
+    def test_should_accept_eof_error_and_perform_a_system_exit(self, setup_teardown, monkeypatch):
+        chat_session = setup_teardown
+        # TODO monkeypatch does not work this way, fix it
+        monkeypatch.setattr('builtins.input', lambda _: EOFError)
+        with pytest.raises(SystemExit):
+            chat_session._prompt_user()
+        assert True
