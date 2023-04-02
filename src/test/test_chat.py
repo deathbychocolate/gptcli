50ae02fb93f5f066970a18120ebbb6b4280ce360
Damianos Vonapartis <damianosvon@gmail.com>
2023-04-02 17:46:08 +0200

    clean(test_chat): remove tests and add real ones

--- src/test/test_chat.py
+++ src/test/test_chat.py
@@ -13,24 +13,16 @@ class TestChat:
     def setup_teardown(self):
         gpt_3_5 = OpenAIHelper.GPT_ALL[0]
         chat_session = Chat(model=gpt_3_5)
+
         yield chat_session
 
-    def test_should_configure_chat_to_enable_arrow_keys(self, setup_teardown):
-        chat_session = setup_teardown()
-        chat_session
-        assert False
-
-    def test_start_method_should_handle_EOFError_input_by_exiting_loop_gracefully(self, setup_teardown, monkeypatch):
-        monkeypatch.setattr('builtins.input', lambda _: EOFError)
+    def test_handle_chat_commands_should_accept_an_exit_string_and_perform_a_system_exit(self, setup_teardown):
         chat_session = setup_teardown
-        result = input('Enter your name: ')
-        assert result == 'user_input'
+        with pytest.raises(SystemExit):
+            chat_session._handle_chat_commands(user_input="exit")
+        assert True
 
-    def test_start_method_should_handle_KeyboardInterrupt_input_by_exiting_loop_gracefully(self, setup_teardown):
-        assert False
-
-    def test_start_method_should_handle_no_input_by_creating_a_clean_prompt(self, setup_teardown):
-        assert False
-
-    def test_start_method_should_handle_EOF_input_by_exiting_loop_gracefully(self, setup_teardown):
-        assert False
+    def test_handle_chat_commands_should_accept_an_empty_string_and_not_perform_a_system_exit(self, setup_teardown):
+        chat_session = setup_teardown
+        chat_session._handle_chat_commands(user_input="")
+        assert True
