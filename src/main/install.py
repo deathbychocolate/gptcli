0591fdcab27270c41c9c4c1188a9509f2d09730c
Damian Vonapartis <damianosvon@gmail.com>
2023-04-06 23:55:40 +0200

    feat(install): add methods for installing locally

--- src/main/install.py
+++ src/main/install.py
@@ -1,10 +1,103 @@
 """
 Use this to install or configure the local machine environment
 """
+import os
+import logging
+
+import requests
+
 
 class Install:
-    """
-    Class to hold all the install tools for the local machine
-    """
+    """Class to hold all the install tools for the local machine"""
+
     def __init__(self) -> None:
-        pass
+        self.home_directory = os.path.expanduser("~")
+        self.gptcli_filepath = "/".join([self.home_directory, ".gptcli"])
+        self.keys_filepath = "/".join([self.gptcli_filepath, "keys"])
+        self.openai_filepath = "/".join([self.keys_filepath, "openai"])
+
+    def is_gptcli_folder_present(self) -> bool:
+        """Checks to see if the .gptcli is present in the home directory"""
+
+        logging.info("Checking for .gptcli")
+        is_present = os.path.exists(self.gptcli_filepath)
+
+        return is_present
+
+    def is_keys_folder_present(self) -> bool:
+        """Checks to see if the keys folder is present under the .gptcli folder"""
+
+        logging.info("Checking for .gptcli/keys")
+        is_present = os.path.exists(self.keys_filepath)
+
+        return is_present
+
+    def is_openai_file_present(self) -> bool:
+        """Checks to see if the openai file is present under the .gptcli/keys folder"""
+
+        logging.info("Checking for .gptcli/keys/openai")
+        is_present = False
+        try:
+            with open(self.openai_filepath, "r", encoding="utf8"):
+                is_present = True
+        except FileNotFoundError:
+            is_present = False
+
+        return is_present
+
+    def openai_contains_one_line(self) -> bool:
+        """Checks to see if the file openai contains one line"""
+
+        logging.info("Checking if openai has one line")
+        home_directory = os.path.expanduser("~")
+        openai_file = "/".join([home_directory, ".gptcli/keys/openai"])
+        contains_one_line = False
+        with open(openai_file, "r", encoding="utf8") as filepointer:
+            lines = filepointer.readlines()
+            if len(lines) == 1:
+                contains_one_line = True
+
+        return contains_one_line
+
+    def is_openai_file_populated_with_a_valid_api_key(self) -> bool:
+        """Checks if API key is valid by performing a cheap (100 token) chat completion.
+        to https://api.openai.com/v1/completions"""
+
+        request_url = "https://api.openai.com/v1/completions"
+        request_headers = {
+            "Accept": "text/event-stream",
+            "Authorization": " ".join(["Bearer", os.getenv("OPENAI_API_KEY")]),
+        }
+        request_body = {
+            "model": "text-davinci-003",
+            "prompt": "Hi!",
+            "max_tokens": 100,
+            "temperature": 0,
+            "stream": True,
+        }
+
+        is_valid_api_key = False
+        try:
+            requests.post(
+                request_url,
+                stream=True,
+                headers=request_headers,
+                json=request_body,
+                timeout=3,  # seconds
+            )
+            is_valid_api_key = True
+        except TimeoutError:
+            is_valid_api_key = False
+
+        return is_valid_api_key
+
+    def load_api_key_to_environment_variable(self) -> None:
+        """Loads API key to environment variable OPENAI_API_KEY"""
+
+        logging.info("Loading API key to environment variable")
+        api_key = os.getenv("OPENAI_API_KEY")
+        if api_key is None:
+            with open(self.openai_filepath, "r", encoding="utf8") as filepointer:
+                os.environ["OPENAI_API_KEY"] = filepointer.readline()
+        else:
+            logging.warning("API key already loaded")
--- src/test/test_install.py
+++ src/test/test_install.py
@@ -1,10 +1,37 @@
 """
 Holds the tests for the machine file
 """
+import pytest
+
+from src.main.install import Install
 
 class TestInstall:
-    """
-    template
-    """
-    def test_should_do_something(self):
-        assert False
+    @pytest.fixture(scope="session")
+    def setup_teardown(self):
+        install = Install()
+        return install
+
+    def test_should_detect_if_the_hidden_folder_gptcli_is_present_in_the_home_directory(self, setup_teardown):
+        install = setup_teardown
+        is_present = install.is_gptcli_folder_present()
+        assert is_present, "Hidden folder gtpcli not found in home dir"
+
+    def test_should_detect_if_the_keys_folder_is_present_under_the_gptcli_folder(self, setup_teardown):
+        install = setup_teardown
+        is_present = install.is_keys_folder_present()
+        assert is_present, "keys folder not found in .gptcli"
+
+    def test_should_detect_if_the_openai_file_is_present_under_the_keys_folder(self, setup_teardown):
+        install = setup_teardown
+        is_present = install.is_openai_file_present()
+        assert is_present, "openai file not found in keys folder"
+
+    def test_should_detect_if_the_openai_file_contains_one_line(self, setup_teardown):
+        install = setup_teardown
+        contains_one_line = install.openai_contains_one_line()
+        assert contains_one_line, "openai file does not contain one line"
+
+    def test_should_detect_if_the_openai_file_is_populated_with_a_valid_api_key(self, setup_teardown):
+        install = setup_teardown
+        is_valid_api_key = install.is_openai_file_populated_with_a_valid_api_key()
+        assert False, "openai file does not contain a valid API key"
