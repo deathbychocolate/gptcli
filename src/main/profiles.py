e890ddc8f15a20f39bc9561b22f2331b9d06e022
Damian Vonapartis <damianosvon@gmail.com>
2023-04-09 21:44:39 +0200

    feat(profiles): add some context and examples

---
+++ src/main/profiles.py
@@ -0,0 +1,12 @@
+"""A profile may be used to receive a desired response from chatGPT
+For example, if you want bash script one liners you may use bashscript.
+Or if you want to enter chatGPT's developer mode, you may user developer.
+"""
+
+DEVELOPER = "enter prompt here"
+BASHSCRIPT = "enter prompt here"
+
+ALL = [
+    DEVELOPER,
+    BASHSCRIPT,
+]
