7f70f775f8b2d365ac505d16542c955f4e99fc17
Damian Vonapartis <damianosvon@gmail.com>
2023-04-09 20:40:42 +0200

    feat(messages): add message store and read support

---
+++ src/main/messages.py
@@ -0,0 +1,11 @@
+"""Allows user to store and open messages in local machine"""
+
+import os
+import logging
+
+
+class Store:
+    """Will store messages"""
+
+class Open:
+    """Will open messages to use for context"""
