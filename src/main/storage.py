00c79fc0df546a44b788a0abde705f794da52271
Damian Vonapartis <damianosvon@gmail.com>
2023-04-23 21:40:56 +0200

    feat(storage): add more basic functionality

--- src/main/storage.py
+++ src/main/storage.py
@@ -2,33 +2,55 @@
 import os
 import logging
 
+from uuid import uuid4
+from datetime import datetime
+from src.main.api_helper import Message
 
 logger = logging.getLogger("__name__")
 
 
-class Storage:
-    """Handles storage"""
-
-    def __init__(self) -> None:
-        pass
-
-
-class Mesages(Storage):
+class Chats():
     """Handles storage of Messages"""
 
     def __init__(self) -> None:
-        Storage.__init__(self)
-
         self.home_directory = os.path.expanduser("~")
         self.messages_filepath = os.path.join(self.home_directory, "messages")
+        self.chat_name = "_".join(
+            [
+                "gptcli",
+                datetime.now().strftime(r'%Y_%m_%d'),
+                str(uuid4()).split("-", maxsplit=1)[0],
+                ".json",
+            ]
+        )
+        self.chat_filepath = os.path.join(self.messages_filepath, self.chat_name)
 
-    def store_messages(self) -> None:
-        """Will store more than 1 message"""
+    def store_messages(self, messages: list[Message]) -> None:
+        """Will store multiple message"""
         logger.info("Storing messages")
+        # TODO add condition for checking if file exists, add comma or not at the end
+        for message in messages:
+            self._store_message(message)
 
-    def store_message(self) -> None:
-        """Will store 1 message"""
-        logger.info("Storing 1 message")
+    def _store_message(self, message: Message) -> None:
+        logger.info("Storing message")
+        # TODO add condition for checking if file exists
+        with open(self.messages_filepath, "a", encoding="utf8") as filepointer:
+            filepointer.write(message.__str__)
+
+    def _is_file_present(self) -> bool:
+        is_present = False
+        try:
+            with open(self.chat_filepath, "r", encoding="utf8"):
+                pass
+        except FileNotFoundError:
+            logger.info("Chat not found: %s", self.chat_filepath)
+        return is_present
+
+    def _create_file(self) -> None:
+        logger.info("Creating file for storage of message(s)")
+        with open(self.messages_filepath, "w", encoding="utf8"):
+            pass
 
     def open_history(self) -> None:
         """Will access"""
