b615b5506498b47dbf15fcbc4b2039ea40c098f6
Damian Vonapartis <damianosvon@gmail.com>
2023-05-06 00:31:47 +0200

    feat(storage)wip: add chat name class + propeties

--- src/main/storage.py
+++ src/main/storage.py
@@ -9,21 +9,61 @@ from src.main.api_helper import Message
 logger = logging.getLogger("__name__")
 
 
-class Chats:
-    """Handles storage of Messages"""
-
+class ChatName:
+    """Used to generate names for Chats"""
     def __init__(self) -> None:
-        self.home_directory = os.path.expanduser("~")
-        self.messages_filepath = os.path.join(self.home_directory, "messages")
-        self.chat_name = "_".join(
+        self.name = self._generate_chat_name()
+
+    def _generate_chat_name(self) -> str:
+        basename = self._generate_chat_basename()
+        file_extension = self._generate_chat_file_extension()
+        chat_name = ".".join(
             [
-                "gptcli",
-                datetime.now().strftime(r"%Y_%m_%d__%H_%M_%S"),
-                str(uuid4()).split("-", maxsplit=1)[0],
-                ".json",
+                basename,
+                file_extension,
             ]
         )
-        self.chat_filepath = os.path.join(self.messages_filepath, self.chat_name)
+
+        return chat_name
+
+    def _generate_chat_file_extension(self) -> str:
+        return "json"
+
+    def _generate_chat_basename(self) -> str:
+        basename = "_".join(
+            [
+                "chat",
+                self._generate_chat_date(),
+                self._generate_chat_random_id(),
+            ]
+        )
+        return basename
+
+    def _generate_chat_date(self) -> str:
+        date = datetime.now().strftime(r"_%Y_%m_%d__%H_%M_%S_")
+        return date
+
+    def _generate_chat_random_id(self) -> str:
+        random_id = str(uuid4()).split("-", maxsplit=1)[0]
+        return random_id
+
+
+class Chat:
+    """Specifically handles Chat storage"""
+
+    def __init__(self) -> None:
+        self._home_directory = os.path.expanduser("~")
+        self._messages_filepath = os.path.join(self._home_directory, "messages")
+        self._name = ChatName().name
+        self._filepath = os.path.join(self._messages_filepath, self._name)
+
+    @property
+    def filepath(self) -> str:
+        return self._filepath
+
+    @property
+    def name(self) -> str:
+        return self._name
 
     def store_messages(self, messages: list[Message]) -> None:
         """Will store multiple message"""
@@ -35,21 +75,21 @@ class Chats:
     def _store_message(self, message: Message) -> None:
         logger.info("Storing message")
         # TODO add condition for checking if file exists
-        with open(self.messages_filepath, "a", encoding="utf8") as filepointer:
+        with open(self._messages_filepath, "a", encoding="utf8") as filepointer:
             filepointer.write(message.__str__)
 
     def _is_file_present(self) -> bool:
         is_present = False
         try:
-            with open(self.chat_filepath, "r", encoding="utf8"):
+            with open(self.filepath, "r", encoding="utf8"):
                 pass
         except FileNotFoundError:
-            logger.info("Chat not found: %s", self.chat_filepath)
+            logger.info("Chat not found: %s", self.filepath)
         return is_present
 
     def _create_file(self) -> None:
         logger.info("Creating file for storage of message(s)")
-        with open(self.messages_filepath, "w", encoding="utf8"):
+        with open(self._messages_filepath, "w", encoding="utf8"):
             pass
 
     def open_history(self) -> None:
