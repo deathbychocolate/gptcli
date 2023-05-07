7edd216ec735212b80ca1061c093f87e105584fb
Damian Vonapartis <damianosvon@gmail.com>
2023-05-07 21:23:42 +0200

    clean(chat): clean and use Messages class to manage sent and received messages

--- src/main/chat.py
+++ src/main/chat.py
@@ -16,7 +16,7 @@ from requests.exceptions import ChunkedEncodingError
 import sseclient
 
 from src.main.api_helper import OpenAIHelper
-from src.main.message import Message
+from src.main.message import Message, Messages
 
 logger = logging.getLogger(__name__)
 
@@ -84,16 +84,20 @@ class ChatOpenai(Chat):
 
         self._model = model
         self._stream = True if stream == "on" else False
-        self._messages: List[Dict] = [Message("user", "").to_dictionary()]
+        self._messages = Messages()
 
     def start(self) -> None:
-        """Will start the chat session that allows USER to AI communication (like texting)"""
+        """Start a chat session. Expect to see the following output to terminal:
+
+        >>> [MESSAGE]: hi
+        >>> [REPLY, model=gpt-4]: Hello! How can I help you today?
+        >>> [MESSAGE]:
+        """
         logger.info("Starting chat")
 
         exit_commands = set(["exit", "q"])
         while True:
             user_input = self.prompt(">>> [MESSAGE]: ")
-
             if len(user_input) != 0:
                 # handle chat commands
                 if user_input in exit_commands:
@@ -101,33 +105,32 @@ class ChatOpenai(Chat):
                 elif user_input.startswith("!"):
                     logger.info("Chat command detected")
                     chat_command = user_input.split("!", maxsplit=1)[1]
-                    if chat_command == "set context on":
-                        self._print_gptcli_message("CONTEXT ON")
-                        continue  # TODO: FEATURE: for when we add context parameter
-                    elif chat_command == "set context off":
-                        self._print_gptcli_message("CONTEXT OFF")
-                        continue  # TODO: FEATURE: for when we add context parameter
-                    elif chat_command.startswith("set model "):
-                        self._print_gptcli_message("MODEL model")
-                        continue  # TODO: FEATURE: add dynamic model switching
+                    if chat_command == "flush":
+                        self.messages = []
+                        self._print_gptcli_message("MESSAGES FLUSHED")
+                        continue
                     else:
                         self._print_gptcli_message("UNKNOWN COMMAND DETECTED")
                 else:
-                    message = Message(role="user", content=user_input).to_dictionary()
-                    self.messages.append(message)
-                    response = OpenAIHelper(self._model, payload=self.messages, stream=self.stream).send()
-                    message = self._reply(response, stream=self.stream).to_dictionary()
-                    self.messages.append(message)
+                    # build and add message to messages
+                    message = Message(role="user", content=user_input)
+                    self.messages.add_message(message)
+
+                    # send message
+                    response = OpenAIHelper(self._model, payload=self.messages.messages , stream=self.stream).send()
+
+                    # add reply
+                    message = self._reply(response, stream=self.stream)
+                    self.messages.add_message(message)
+                    print(self.messages.messages)
 
     def _reply(self, response: Response, stream: bool) -> Message:
         logger.info("Selecting reply mode")
-
         message: Message = None
         if response is None:
             self._print_none()
         else:
             if stream:
-                # self._print_fast(response)
                 message = self._print_stream(response)
             else:
                 message = self._print_simple(response)
@@ -154,60 +157,21 @@ class ChatOpenai(Chat):
                         payload = "".join([payload, text])
             message.content = payload
             print("")
-
         except ChunkedEncodingError:
-            print("")  # newline
+            print("")
             self._print_gptcli_message("ChunkedEncodingError detected. Maybe try again.")
         except KeyboardInterrupt:
-            print("")  # newline
+            print("")
             self._print_gptcli_message("KeyboardInterrupt detected")
 
         return message
 
-    def _print_fast(self, response: Response) -> Message:
-        # TODO: FIX: this option is just as slow (if not slower) than the reply_simple method. Try using Rust to do this process.
-        # TODO: FEATURE: make this mode available for no chat mode.
-        """
-        b'data: {"id":"chatcmpl-74cA0XBe8E7kMcyi0B9J8CKSJtXng","object":"chat.completion.chunk","created":1681334356,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"role":"assistant"},"index":0,"finish_reason":null}]}
-        data: {"id":"chatcmpl-74cA0XBe8E7kMcyi0B9J8CKSJtXng","object":"chat.completion.chunk","created":1681334356,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":"Hello"},"index":0,"finish_reason":null}]}
-        data: {"id":"chatcmpl-74cA0XBe8E7kMcyi0B9J8CKSJtXng","object":"chat.completion.chunk","created":1681334356,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":"!"},"index":0,"finish_reason":null}]}
-        data: {"id":"chatcmpl-74cA0XBe8E7kMcyi0B9J8CKSJtXng","object":"chat.completion.chunk","created":1681334356,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":" How"},"index":0,"finish_reason":null}]}
-        data: {"id":"chatcmpl-74cA0XBe8E7kMcyi0B9J8CKSJtXng","object":"chat.completion.chunk","created":1681334356,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":" may"},"index":0,"finish_reason":null}]}
-        data: {"id":"chatcmpl-74cA0XBe8E7kMcyi0B9J8CKSJtXng","object":"chat.completion.chunk","created":1681334356,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":" I"},"index":0,"finish_reason":null}]}
-        data: {"id":"chatcmpl-74cA0XBe8E7kMcyi0B9J8CKSJtXng","object":"chat.completion.chunk","created":1681334356,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":" assist"},"index":0,"finish_reason":null}]}
-        data: {"id":"chatcmpl-74cA0XBe8E7kMcyi0B9J8CKSJtXng","object":"chat.completion.chunk","created":1681334356,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":" you"},"index":0,"finish_reason":null}]}
-        data: {"id":"chatcmpl-74cA0XBe8E7kMcyi0B9J8CKSJtXng","object":"chat.completion.chunk","created":1681334356,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":" today"},"index":0,"finish_reason":null}]}
-        data: {"id":"chatcmpl-74cA0XBe8E7kMcyi0B9J8CKSJtXng","object":"chat.completion.chunk","created":1681334356,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":"?"},"index":0,"finish_reason":null}]}
-        data: {"id":"chatcmpl-74cA0XBe8E7kMcyi0B9J8CKSJtXng","object":"chat.completion.chunk","created":1681334356,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{},"index":0,"finish_reason":"stop"}]}
-        data: [DONE]
-        '
-        """
-        logger.info("Reply mode -> Fast")
-
-        # clean response content to json ready format
-        content = response.content.decode("UTF8")
-        content = content.replace("data: ", "")
-        content = content.split("\n\n")
-        content = content[1:-3]
-
-        # collect all server sent events text into chucks list
-        chunks = []
-        for data in content:
-            chunk = json.loads(data)
-            chunk = chunk["choices"][0]["delta"]["content"]
-            chunks.append(chunk)
-
-        # concatenate the chunks into a full reply
-        reply = "".join(chunks)
-
-        self._print_reply(reply)
-
     def _print_simple(self, response: Response) -> Message:
-        # TODO: FEATURE: make this mode available for no chat mode.
         logger.info("Reply mode -> Simple")
-        text = json.loads(response.content)["choices"][0]["message"]["content"]
-        self._print_reply(text)
-        message = Message(role="user", content=text)
+        content = json.loads(response.content)["choices"][0]["message"]["content"]
+        self._print_reply(content)
+        message = Message(role="user", content=content)
+
         return message
 
     def _print_reply(self, text: str, end="\n") -> None:
@@ -226,3 +190,7 @@ class ChatOpenai(Chat):
     @property
     def messages(self) -> List[Dict]:
         return self._messages
+
+    @messages.setter
+    def messages(self, value):
+        self._messages = value
