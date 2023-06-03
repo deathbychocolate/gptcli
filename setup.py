b75ec81224cae7704b9d6358a31b4d1fae50a12e
Damianos Vonapartis <damianosvon@gmail.com>
2023-06-03 19:22:10 +0200

    feat(project): :construction: Add initial setup.py version. Contains program entry point.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="gptcli",
+    version="0.1.0",
+    description="A CLI tool for talking to the Openai API.",
+    author="Damian Vonapartis",
+    url="https://www.python.org/sigs/distutils-sig/",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["gptcli = gptcli.main:main"]},
+)
