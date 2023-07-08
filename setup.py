d8f23c19a0ae95ae6bca7fa7e3909f8eb1299cfd
Damian Vonapartis <damianosvon@gmail.com>
2023-07-08 23:48:12 +0200

    fix(project): :bug: Replace hardcoded version number.

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 
+import configparser
+
 from setuptools import find_packages, setup
+
+
+def _get_project_version_number()->str:
+    config = configparser.ConfigParser()
+    project_version_number = config.read("pyproject.toml", "version")
+    project_version_number = project_version_number.strip('"')
+    return project_version_number
 
 setup(
     name="gptcli",
-    version="0.1.0",
+    version=_get_project_version_number(),
     description="A CLI tool for talking to the Openai API.",
     author="Damian Vonapartis",
     url="https://www.python.org/sigs/distutils-sig/",
