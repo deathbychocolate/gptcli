49b0a8af3add5ea52217c09359a66bcd9ae44804
Damian Vonapartis <damianosvon@gmail.com>
2023-07-15 20:31:23 +0200

    docs(profiles): Fix doc string.

--- gptcli/src/profiles.py
+++ gptcli/src/profiles.py
@@ -1,4 +1,4 @@
-"""A profile may be used to receive a desired response from chatGPT
+"""A profile may be used to receive a desired response from the Large Language Model (LLM).
 For example, if you want bash script one liners you may use bashscript.
 Or if you want to enter chatGPT's developer mode, you may user developer.
 """
