c0f4bd2aefdad1d0e5ee9b75d426fb1405a6fd51
Damianos Vonapartis <damianosvon@gmail.com>
2024-05-31 00:06:54 +0200

    refactor(project): Add commonly used filepath to definitions.

--- gptcli/definitions.py
+++ gptcli/definitions.py
@@ -1,4 +1,6 @@
 """This file holds common definitions/constants used throughout the project"""
 import os
 
-PROJECT_ROOT_DIRECTORY = os.getcwd()
+PROJECT_ROOT_DIRECTORY: str = os.getcwd()
+
+GPTCLI_STORAGE_FILEPATH: str = "~/.gptcli/storage"
