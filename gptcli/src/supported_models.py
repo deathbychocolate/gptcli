d152625aa4fa879181091812f5f537e04c85f801
Damianos Vonapartis <damianosvon@gmail.com>
2024-06-15 21:04:06 +0200

    refactor(project): Remove deprecated and soon to be deprecated LLMs.

--- gptcli/src/supported_models.py
+++ gptcli/src/supported_models.py
@@ -3,12 +3,7 @@
 openai: dict = {
     # see here: https://platform.openai.com/docs/models/
     "GPT_3_5_TURBO": "gpt-3.5-turbo",
-    "GPT_3_5_301": "gpt-3.5-turbo-0301",
-    "GPT_3_5_16K": "gpt-3.5-turbo-16k",
     "GPT_4": "gpt-4",
     "GPT_4_TURBO": "gpt-4-turbo",
-    "GPT_4_32K": "gpt-4-32k",
-    "GPT_4_0314": "gpt-4-0314",
-    "GPT_4_32K_0314": "gpt-4-32k-0314",
     "GPT_4O": "gpt-4o",
 }
