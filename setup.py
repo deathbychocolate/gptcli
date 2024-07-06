6c48522127b116b92d9070747195f859e43c8b66
Damianos Vonapartis <damianosvon@gmail.com>
2024-07-06 02:47:29 +0200

    fix(project): Fix missing module error with pypdf and move all configurations to toml from setup.py.

--- setup.py
+++ setup.py
@@ -4,12 +4,4 @@ from setuptools import find_packages, setup
 
 __version__ = "0.16.1"
 
-setup(
-    name="gptcli",
-    version=__version__,
-    description="A CLI tool for talking to the Openai API.",
-    author="Damian Vonapartis",
-    url="https://www.python.org/sigs/distutils-sig/",
-    packages=find_packages(exclude=["*tests*"]),
-    entry_points={"console_scripts": ["gptcli = gptcli.main:main"]},
-)
+setup(packages=find_packages(exclude=["*tests*"]))
