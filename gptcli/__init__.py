f864e15cbcf60acae1d5fe3f1e5a037702ab3c54
Damianos Vonapartis <damianosvon@gmail.com>
2024-07-31 21:21:07 +0200

    chore: Add some rules to vscode config files and apply formatting to private files

--- gptcli/__init__.py
+++ gptcli/__init__.py
@@ -1,2 +1,3 @@
 """This file is needed to make the project compatible with pypi."""
+
 from .main import main
--- gptcli/_version.py
+++ gptcli/_version.py
@@ -1,2 +1,3 @@
 """A file that holds only the version number"""
+
 __version__ = "0.17.0"
