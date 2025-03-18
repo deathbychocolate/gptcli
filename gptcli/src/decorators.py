562f279593fe81bd6796318b5bab2531457dc612
Damianos Vonapartis <damianosvon@gmail.com>
2025-03-18 12:26:02 -0700

    docs(decorators): Add some basic documentation.

--- gptcli/src/decorators.py
+++ gptcli/src/decorators.py
@@ -3,23 +3,32 @@
 import logging
 import sys
 from logging import Logger
+from typing import Any, Callable
 
 logger: Logger = logging.getLogger(__name__)
 
 
-def user_triggered_abort(function):
+def user_triggered_abort(function: Callable[..., Any]):
+    """Use this wrapper to catch KeyboardInterrupt or EOFError errors.
+
+    This is useful if for example the user decides to use Ctrl+C to
+    exit the program entirely.
+
+    Args:
+        function (Callable[..., Any]): The method or function to wrap around.
+    """
 
     def decorator(*args, **kwargs):
         logger.info("Calling '%s', with args '%s', and kwargs '%s'", function.__qualname__, args, kwargs)
         try:
             return function(*args, **kwargs)
         except KeyboardInterrupt as exception:
-            print()
+            print()  # add new line
             logger.info("'KeyboardInterrupt' detected, exiting program.")
             logger.exception(exception)
             sys.exit()
         except EOFError as exception:
-            print()
+            print()  # add new line
             logger.info("'EOFError' detected, exiting program.")
             logger.exception(exception)
             sys.exit()
@@ -27,18 +36,26 @@ def user_triggered_abort(function):
     return decorator
 
 
-def allow_graceful_stream_exit(function):
+def allow_graceful_stream_exit(function: Callable[..., Any]):
+    """Use this wrapper to catch KeyboardInterrupt or EOFError errors.
+
+    This is useful if for example the user decides to use Ctrl+C to
+    exit the printing of text when using streaming mode.
+
+    Args:
+        function (Callable[..., Any]): The method or function to wrap around.
+    """
 
     def decorator(*args, **kwargs):
         logger.info("Calling '%s', with args '%s', and kwargs '%s'", function.__qualname__, args, kwargs)
         try:
             return function(*args, **kwargs)
         except KeyboardInterrupt as exception:
-            print()
+            print()  # add new line
             logger.info("'KeyboardInterrupt' detected, exiting chat.")
             logger.exception(exception)
         except EOFError as exception:
-            print()
+            print()  # add new line
             logger.info("'EOFError' detected, exiting chat.")
             logger.exception(exception)
 
