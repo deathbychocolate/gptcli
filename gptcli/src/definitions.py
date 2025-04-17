58ab349f68d9dc9f66c67eaabcddaa1cc762ba7a
Damianos Vonapartis <damianosvon@gmail.com>
2025-04-17 15:05:34 +0200

    feat: Add new models.

--- gptcli/src/definitions.py
+++ gptcli/src/definitions.py
@@ -5,6 +5,8 @@ openai: dict[str, str] = {
     "GPT_3_5_TURBO": "gpt-3.5-turbo",
     "GPT_4": "gpt-4",
     "GPT_4_TURBO": "gpt-4-turbo",
+    "GPT_4_1": "gpt-4.1",
+    "GPT_4_1_MINI": "gpt-4.1-mini",
     "GPT_4O": "gpt-4o",
     "GPT_4O_MINI": "gpt-4o-mini",
     "O1": "o1",
@@ -12,6 +14,8 @@ openai: dict[str, str] = {
     "O1_PREVIEW": "o1-preview",
     "O1": "o1",
     "O3_MINI": "o3-mini",
+    "O3": "o3",
+    "O4_MINI": "o4-mini",
 }
 
 roles: list[str] = [
